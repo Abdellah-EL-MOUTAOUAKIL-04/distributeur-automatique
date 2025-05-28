f1098c69a03b7f00d831c62102b7a21844fa2c82
Abdellah EL MOUTAOUKIL <abdamota123@gmail.com>
2025-05-28 19:46:22 +0100

    initial commit

---
+++ src/main/java/ma/abdellahelmoutaouakil/distributeurautomatique/DistributeurAutomatiqueApplication.java
@@ -0,0 +1,13 @@
+package ma.abdellahelmoutaouakil.distributeurautomatique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributeurAutomatiqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributeurAutomatiqueApplication.class, args);
+    }
+
+}
